6fdd21750648be05caa864d0b21914166ac5fb5b
bonheurdev <irbonheur3@gmail.com>
2023-08-31 05:55:59 -0700

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,32 +11,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-if (parent == NULL)
-{
-return (NULL); /* Return NULL if parent is NULL */
-}
+binary_tree_t *new_node;
 
-binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-if (new_node == NULL)
-{
-return (NULL); /* Return NULL on memory allocation failure */
-}
+if (!parent)
+return (NULL);/*Return NULL if parent is NULL*/
+
+new_node = malloc(sizeof(binary_tree_t));
+if (!new_node)
+return (NULL);/*Return NULL on memory allocation failure*/
 
 new_node->n = value;
 new_node->parent = parent;
 new_node->right = NULL;
-
-if (parent->left != NULL)
-{
 new_node->left = parent->left;
-parent->left->parent = new_node;
-}
-else
-{
-new_node->left = NULL;
-}
-
 parent->left = new_node;
-
+if (new_node->left)
+new_node->left->parent = new_node;
 return (new_node);
 }
